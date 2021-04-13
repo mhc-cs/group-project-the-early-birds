f6ff4ae070e6098453a58444b9fbbe00a880b6c6
brown27h <brown27h@mtholyoke.edu>
2021-04-13 09:00:45 -0700

    Started card class (testing commits)

--- GuessWho/src/guesswho/Card.java
+++ GuessWho/src/guesswho/Card.java
@@ -1,5 +1,17 @@
 package guesswho;
 
-public class Card {
 
+public class Card {
+	private String cardName;
+	private String imagePath;
+	private boolean grey;
+	
+	public Card(String name) {
+		cardName = name;
+		grey = false;
+		imagePath = "defaultImages/defaultNoImage.png";
+	}
+	
+	
+	
 }
