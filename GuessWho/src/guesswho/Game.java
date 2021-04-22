fa703e30b6a71c3ea7d328af4804512c9f62fa7d
brown27h <brown27h@mtholyoke.edu>
2021-04-21 23:20:37 -0700

    Added comments

--- GuessWho/src/guesswho/Game.java
+++ GuessWho/src/guesswho/Game.java
@@ -2,18 +2,39 @@ package guesswho;
 
 import java.util.Random;
 
+/**
+ * Game
+ * has a deck and two players with functions for game actions
+ * @author Hannah, Dani, Anna
+ *
+ */
 public class Game {
 
+	//deck of cards for game
 	private Deck deck;
+	
+	//one player in game
 	private Player player1;
+	
+	//second player in game
 	private Player player2;
 	
+	/**
+	 * Game constructor
+	 * @param d deck of cards
+	 * @param p1 first player
+	 * @param p2 second player
+	 */
 	public Game(Deck d, Player p1, Player p2) {
 		deck = d;
 		player1 = p1;
 		player2 = p2;	
 	}
 	
+	/*
+	 * draw cards for both players
+	 * cannot both have the same card
+	 */
 	public void drawCards() {
 		Card c1 = deck.drawRandomCard();
 		Card c2 = deck.drawRandomCard();
@@ -24,6 +45,9 @@ public class Game {
 		player2.setCard(c2);
 	}
 	
+	/**
+	 * randomly assign which player goes first
+	 */
 	public void assignFirstTurn() {
 		Random r = new Random();
 	    int chance = r.nextInt(2);
@@ -36,16 +60,30 @@ public class Game {
 	    }
 	}
 	
+	/**
+	 * Grey out a card (or un-grey)
+	 * @param index of the card in the deck
+	 */
 	public void toggleGreyCard(int index) {
 		Card c = deck.getCard(index);
 		c.toggleGrey();
 	}
 	
+	/**
+	 * Change which player's turn it is
+	 */
 	public void endTurn() {
 		player1.toggleTurn();
 		player2.toggleTurn();
 	}
 	
+	/**
+	 * Guess other player's card
+	 * increase score if cards match
+	 * must be guessing player's turn
+	 * if incorrect end turn
+	 * @param c card to guess
+	 */
 	public void guess(Card c) {
 		if (player1.getTurn()){
 			if(c==player2.getCard()) {
