fef47f60d920dea9952bd7e11a5a3e4d40f0a5fe
Dani L <levy22d@mtholyoke.edu>
2021-05-01 15:45:43 -0500

    Opens when "yes" is pressed (for now)

--- GuessWho/src/application/ConfirmationMenuController.java
+++ GuessWho/src/application/ConfirmationMenuController.java
@@ -1,7 +1,13 @@
 package application;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
@@ -26,7 +32,25 @@ public class ConfirmationMenuController {
      * Happens when the "yes" button is pressed in the confirmation
      * menu. 
      */
-    public void confirm() {
+    public void confirm(ActionEvent event) {
         //TODO implement
+        
+        try {
+            //Loads the new screen
+            Parent startGameParent = FXMLLoader.load(getClass().getResource("NewRoundScreen.fxml"));
+            Scene startGameScene = new Scene(startGameParent);
+            
+            //Finds the previous screen and switches off of it
+            Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            appStage.setScene(startGameScene);
+            appStage.centerOnScreen();
+            
+            //Shows the new screen
+            appStage.show();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        
+        }
     }
 }
