d8a7180df98fe23a78a4c34ca27066f28006c6a6
Dani L <levy22d@mtholyoke.edu>
2021-05-01 14:19:22 -0500

    redraw screen started

---
+++ GuessWho/src/application/RedrawScreenController.java
@@ -0,0 +1,23 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class RedrawScreenController {
+
+    @FXML
+    private Label cardName;
+    
+    @FXML
+    private ImageView card;
+    
+    public void redraw() {
+        
+    }
+    
+    public void continueButton() {
+        
+    }
+    
+}
