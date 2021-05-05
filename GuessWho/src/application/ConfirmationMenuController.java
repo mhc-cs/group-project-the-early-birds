bf89a636b6ee3ec64c76b1fcd5edc32d654e0f61
Dani L <levy22d@mtholyoke.edu>
2021-05-05 13:05:37 -0500

    fixed confirmation menu error with guess

--- GuessWho/src/application/ConfirmationMenuController.java
+++ GuessWho/src/application/ConfirmationMenuController.java
@@ -81,7 +81,7 @@ public class ConfirmationMenuController extends Controller {
             
             }
         } else {
-            GameplayScreenController controller = HostGamecodeScreenController.controller;
+            GameplayScreenController controller = GameplayScreenController.getController();
             
             //ends turn, mimics pressing the guess button and sets guessing to false
             controller.endTurn();
