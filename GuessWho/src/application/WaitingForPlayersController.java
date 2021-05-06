91bf1e431203ea33368f79d0647cdc404092a86a
Dani L <levy22d@mtholyoke.edu>
2021-05-06 15:03:16 -0500

    Added button to waiting for players screen. Appears after 3 seconds.

--- GuessWho/src/application/WaitingForPlayersController.java
+++ GuessWho/src/application/WaitingForPlayersController.java
@@ -1,9 +1,39 @@
 package application;
 
+import javafx.animation.PauseTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
+/**
+ * Contains methods for the waiting for players screen. Initializes
+ * the screen and defines the action for the button.
+ * 
+ * @author Dani, Hannah, Anna
+ *
+ */
 public class WaitingForPlayersController {
     @FXML
     private Button close;
+    
+    /**
+     * Initializes the scene. The close button will appear after 3 seconds.
+     */
+    public void initialize() {
+        close.setVisible(false);
+        PauseTransition delayButton = new PauseTransition(Duration.seconds(3));
+        delayButton.setOnFinished(e -> close.setVisible(true));
+        delayButton.play();
+    }
+    
+    /**
+     * Allows the user to close the dialog and stop attempting connection to the
+     * other player.
+     */
+    public void closeDialog() {
+        Stage stage = (Stage) close.getScene().getWindow();
+        stage.close();
+        //TODO stop connecting. leave message?
+    }
 }
